a1d122e1964b5f7159f6744080ef6ca97c6ac92d
Przemysław Sosnowski <lampard082@gmail.com>
2020-03-25 02:48:45 +0100

    Updates

---
+++ precache-manifest.9efd820a6cccd9ff715c13616a7c41e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6711083f846064ada801ee12ad0d2489",
+    "url": "/zastrzyk/index.html"
+  },
+  {
+    "revision": "66f76f9e2b98ad16c271",
+    "url": "/zastrzyk/static/css/main.a22cda3e.chunk.css"
+  },
+  {
+    "revision": "770cc4a2e01c63fb12a4",
+    "url": "/zastrzyk/static/js/2.9bb003c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zastrzyk/static/js/2.9bb003c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66f76f9e2b98ad16c271",
+    "url": "/zastrzyk/static/js/main.4dabaa21.chunk.js"
+  },
+  {
+    "revision": "a7f221289c387c3ecdd4",
+    "url": "/zastrzyk/static/js/runtime-main.49c11e78.js"
+  },
+  {
+    "revision": "4e6ff3505ae6d59f9ea1f1416b9c87fb",
+    "url": "/zastrzyk/static/media/notification.4e6ff350.mp3"
+  }
+]);
