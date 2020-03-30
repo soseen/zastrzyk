c280ba52ebc5a476084c93cb98ae1b64de80eace
Przemysław Sosnowski <lampard082@gmail.com>
2020-03-30 02:06:23 +0200

    changed the method of audio notifications

--- src/components/Injection.jsx
+++ src/components/Injection.jsx
@@ -13,25 +13,22 @@ const Injection = ({injection, date, removeInjection}) => {
         if(highlight === false && injectionDone === false && (injection.scheduledHour - date.getHours()) === 1 && date.getMinutes() >= (60 - injection.reminderDuration)){
             setHighlight(true);
             audio.play();
-        } else if (highlight === true && injection.scheduledHour - date.getHours() !== 1){
+        } else if (highlight === true && injection.scheduledHour - date.getHours() <=0){
             setHighlight(false);
         }
     }, [highlight, injectionDone, date, injection]);
 
     useEffect(() => {
-        const interval = setInterval(() => {
             if(highlight){
                 audio.play();
             }
-        }, 60000);
-        return () => clearInterval(interval);
-      }, [highlight]);
+      }, [date.getMinutes()]);
 
       useEffect(() => {
         if(date.getHours() === 0 && date.getMinutes() === 0){
             setInjectionDone(false);
         }
-      }, [date]);
+      }, [date.getHours()]);
 
     const handleInjection = (e) => {
         if(!injectionDone && highlight){
